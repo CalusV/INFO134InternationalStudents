61be59102eb24dc39a2248bd691f22067b17faa8
itnora <32238910+itnora@users.noreply.github.com>
2018-03-16 13:33:07 +0100

    api fetch

--- js/toiletscript.js
+++ js/toiletscript.js
@@ -1,17 +1,49 @@
 /**
  * Script for sending a http request to the server so that
  * we can read and write to the JSON file dokart.json as an
- * javascript object. 
+ * javascript object.
  * Author (Øyvind Johannessen)
  * Version (0.1)
 **/
-var xmlhttp = new XMLHttpRequest(),
-    dokart;
+var myJson=fetch('https://hotell.difi.no/api/json/bergen/dokart?')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+		console.log(myJson.entries[1].plassering);
+ });
 
-xmlhttp.onreadystatechange = function() {
-  if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-    dokart = JSON.parse(xmlhttp.responseText);
-  }
-}
-xmlhttp.open('GET', 'js/dokart.json', true);
-xmlhttp.send();
+ function fetch(){
+ $http.get("https://hotell.difi.no/api/json/bergen/dokart?" + $scope.search + "&perpage=25000")
+ .success(function(response){
+ $scope.songs = [];
+ $scope.details = response;
+
+ for(var i=0; i < response.entries.length; i++){
+
+
+   var allTracks = {
+     latitude: response.entries[i].track_id,
+     longitude: response.entries[i].label_fk,
+     plassering: response.entries[i].album_num,
+     img: imgUrl,
+     url: resultUrl
+   };
+
+   $scope.songs.push(allTracks);
+   }
+   });
+ }
+
+
+var xmlhttp = new XMLHttpRequest(),
+     dokart;
+
+ xmlhttp.onreadystatechange = function() {
+   if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+     dokart = JSON.parse(xmlhttp.responseText);
+   }
+ }
+ xmlhttp.open('GET', 'js/dokart.json', true);
+ xmlhttp.send();
