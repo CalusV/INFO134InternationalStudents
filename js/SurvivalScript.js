d10f3894f2d81af264be8914957d74b9dafe9f5e
itnora <32238910+itnora@users.noreply.github.com>
2018-05-02 12:35:48 +0200

    snart ferdig :D

--- js/SurvivalScript.js
+++ js/SurvivalScript.js
@@ -1,28 +1,28 @@
 /**
- * Function for showing and hiding subNav elements
+* Function for showing and hiding subNav elements
 **/
-var currentSelection = getElementById("description-text");
+
 function hideMe(value) {
-    showOrHideDescriptionText();
-    var x = document.getElementById(value);
+  showOrHideDescriptionText();
+  var x = document.getElementById(value);
 
-    if(typeof currentSelection !== 'undefined') {
-        currentSelection.style.display = "none";
-    }
+  if(typeof currentSelection !== 'undefined') {
+    currentSelection.style.display = "none";
+  }
 
-          x.style.display === "flex";
-      if (x.style.display === "flex") {
-          x.style.display = "none";
-          /*x.style.color = "blue";*/
-      } else {
-          x.style.display = "flex";
-          /*x.style.color= "red";*/
-    }
-    currentSelection = x;
+  x.style.display === "flex";
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    /*x.style.color = "blue";*/
+  } else {
+    x.style.display = "flex";
+    /*x.style.color= "red";*/
+  }
+  currentSelection = x;
 }
 
 /**
- * Function for hiding the description intro text for survival page.
+* Function for hiding the description intro text for survival page.
 **/
 function showOrHideDescriptionText() {
   var dt = document.getElementById("description-text");
